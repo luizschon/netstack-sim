8e68c27af7e4d92cae39c6c34889a4bba1f39299
Luiz Schonarth <luizcschonarth@gmail.com>
2022-08-24 02:41:41 -0300

    Adiciona novos métodos e funções no módulo de Camada Física

--- CamadaFisica.cpp
+++ CamadaFisica.cpp
@@ -1,9 +1,36 @@
-#include <iostream>
 #include "CamadaFisica.hpp"
 
 using namespace camada_fis;
 
-void Binaria::transmitir() {
+void mensagem_para_quadro(std::string mensagem) {
+
+} // fim do método mensagem_para_quadro
+
+Codificacao::Codificacao(std::string mensagem) {
+
+} // fim do método construtor Codificacao
+
+Codificacao::~Codificacao() {
+
+} // fim do método destrutor ~Codificacao
+
+void Codificacao::setQuadro(bit quadro[]) {
+
+} // fim do método setQuadro
+
+void Codificacao::setQuadro(std::string mensagem) {
+
+} // fim do método setQuadro
+
+bit * Codificacao::getQuadro() {
+
+} // fim do método setQuadro
+
+Binaria::Binaria(std::string mensagem) : Codificacao(mensagem) {
+
+} // fim do método construtor Binaria
+  
+void Binaria::codificar() {
 
 } // fim do método Binaria::transmitir
 
@@ -11,7 +38,11 @@ void Binaria::decodificar() {
 
 } // fim do método Binaria::decodificar
 
-void Manchester::transmitir() {
+Manchester::Manchester(std::string mensagem) : Codificacao(mensagem) {
+
+} // fim do método construtor Manchester
+
+void Manchester::codificar() {
 
 } // fim do método Manchester::transmitir
 
@@ -19,10 +50,18 @@ void Manchester::decodificar() {
 
 } // fim do método Manchester::decodificar
 
-void Bipolar::transmitir() {
+Bipolar::Bipolar(std::string mensagem) : Codificacao(mensagem) {
+
+} // fim do método construtor Bipolar
+
+void Bipolar::codificar() {
 
 } // fim do método Bipolar::transmitir
 
 void Bipolar::decodificar() {
 
 } // fim do método Bipolar::decodificar
+
+void meio_comunicacao::transmitir(bit trem_de_bits[]) {
+
+} // fim do método meio_comunicacao::transmitir
--- CamadaFisica.hpp
+++ CamadaFisica.hpp
@@ -1,6 +1,12 @@
 #ifndef __CAMADA_FISICA_HPP
 #define __CAMADA_FISICA_HPP
 
+#include <string>
+
+/* Implementa tipo "bit" usando tipo primitivo bool
+ * (booleano) representando 0 ou 1. */
+typedef bool bit;
+
 /* Enumerador para tornar a seleção do tipo de codificação
  * mais legível no código. */
 enum {
@@ -9,24 +15,40 @@ enum {
 
 namespace camada_fis {
 
+    /* Função de utilidade para transformar mensagem (string) em
+     * um quadro composto de bits. */
+    int* mensagem_para_quadro(std::string mensagem);
+
     /* Classe abstrata para representar uma codificação genérica
      * da camada física da comunicação. */
     class Codificacao {
     public:
-        virtual ~Codificacao() {};
-        virtual void transmitir() = 0;
+        Codificacao(std::string mensagem);
+        Codificacao() = default; 
+        virtual ~Codificacao();
+
+        // Assinatura dos métodos virtuais puros
+        virtual void codificar()   = 0;
         virtual void decodificar() = 0;
+
+        // Setters e getter para o quadro
+        void setQuadro(bit quadro[]);
+        void setQuadro(std::string mensagem);
+        bit* getQuadro();
+
+    protected:
+        bit *quadro = nullptr; // Quadro composto de bits (valor booleano 0 ou 1)
     };
 
     /* Classe que deriva a classe abstrata 'Codificacao' e implementa
      * métodos virtuais seguindo o padrão da codificação binária (NRZ). */
     class Binaria : public Codificacao {
     public:
-        Binaria() {};
-        ~Binaria() {};
+        Binaria(std::string mensagem);
+        Binaria() = default;
+        ~Binaria() = default;
 
-    private:
-        void transmitir() override;
+        void codificar() override;
         void decodificar() override;
     };
 
@@ -35,11 +57,11 @@ namespace camada_fis {
      * (NRZ XOR Clock). */
     class Manchester : public Codificacao {
     public:
-        Manchester() {};
-        ~Manchester() {};
+        Manchester(std::string mensagem);
+        Manchester() = default;
+        ~Manchester() = default;
 
-    private:
-        void transmitir() override;
+        void codificar() override;
         void decodificar() override;
     };
 
@@ -47,14 +69,20 @@ namespace camada_fis {
      * métodos virtuais seguindo o padrão da codificação bipolar (AMI). */
     class Bipolar : public Codificacao {
     public:
-        Bipolar() {};
-        ~Bipolar() {};
+        Bipolar(std::string mensagem);
+        Bipolar() = default;
+        ~Bipolar() = default;
 
-    private:
-        void transmitir() override;
+        void codificar() override;
         void decodificar() override;
     };
 
+}
+
+namespace meio_comunicacao {
+    /* Função que transmite trem de bits da camada física transmissora 
+     * para a camada física receptora. */
+    void transmitir(bit trem_de_bits[]);
 }
 
 #endif // __CAMADA_FISICA_HPP
