51d3b8e29c934ee7f5e9add4aed6c1e763bf047b
Luiz Schonarth <luizcschonarth@gmail.com>
2022-08-24 13:27:11 -0300

    Implementa métodos de Codificacao

--- CamadaFisica.cpp
+++ CamadaFisica.cpp
@@ -1,29 +1,38 @@
+#include <iostream>
 #include "CamadaFisica.hpp"
+#include "Common.hpp"
 
 using namespace camada_fis;
 
-void mensagem_para_quadro(std::string mensagem) {
+std::vector<bit> camada_fis::mensagem_para_quadro(const std::string &mensagem) {
+    std::vector<bit> quadro;
 
+    for (auto &c : mensagem) {
+        for(int i = 0; i < TAM_BYTE; i++) {
+            /* Para cada caractere da mensagem, insere os bits do byte do caracter
+             * na ordem big-endian -> little-endian. */
+            quadro.push_back(((c << i) & 0x80));
+        }
+    }
+    return quadro;
 } // fim do método mensagem_para_quadro
 
-Codificacao::Codificacao(std::string mensagem) {
-
+Codificacao::Codificacao(const std::string &mensagem) {
+    std::cout << "From codificacao: " << mensagem << "\n";
+    this->quadro = mensagem_para_quadro(mensagem);
+    std::cout << "Quadro size: " << quadro.size() << "bits or " << quadro.size()/8 << " bytes\n";
 } // fim do método construtor Codificacao
 
-Codificacao::~Codificacao() {
-
-} // fim do método destrutor ~Codificacao
-
-void Codificacao::setQuadro(bit quadro[]) {
-
+void Codificacao::setQuadro(std::vector<bit> &quadro) {
+    this->quadro = quadro;
 } // fim do método setQuadro
 
-void Codificacao::setQuadro(std::string mensagem) {
-
+void Codificacao::setQuadro(const std::string &mensagem) {
+    this->quadro = mensagem_para_quadro(mensagem);
 } // fim do método setQuadro
 
-bit * Codificacao::getQuadro() {
-
+std::vector<bit> Codificacao::getQuadro() {
+    return quadro;
 } // fim do método setQuadro
 
 Binaria::Binaria(std::string mensagem) : Codificacao(mensagem) {
